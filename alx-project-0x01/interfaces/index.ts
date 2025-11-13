5e802c35eba0340b7f2b28623ff4c99eae0dce52
veenzent <odumevincent19@gmail.com>
2025-11-13 17:43:49 +0100

    Task 5. Implement the User Modal ✅

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -5,6 +5,19 @@ export interface PostProps {
   body: string;
 }
 
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
+
+
 // from this point makes up the UserProps interface
 export interface Geo {
     lat: string;
@@ -61,14 +74,18 @@ export interface UserProps {
 //   }
 // }
 
-export interface PostData {
-  userId: number;
-  id?: number;
-  title: string;
-  body: string;
+export interface UserData {
+    id?: number;
+    name: string;
+    username: string;
+    email: string;
+    address: Address;
+    phone: string;
+    website: string;
+    company: Company;
 }
 
-export interface PostModalProps {
-  onClose: () => void;
-  onSubmit: (post: PostData) => void;
+export interface UserModalProps {
+    onClose: () => void;
+    onSubmit: (user: UserData) => void;
 }
